2bb82a38fc0419d27a325f8781af3d81c169f3c0
Alltxe <148350310+Alltxe@users.noreply.github.com>
2023-12-10 21:11:07 +0300

    Add files via upload

---
+++ Labs/L_11/z1v2.pas
@@ -0,0 +1,26 @@
+﻿program z1v1;
+uses GraphABC;
+var
+  h,w,a,x1,x2,x3,x4,y1,y2,y3,y4: integer;
+begin
+  w:=600;
+  h:=600;
+  SetWindowWidth(W);
+  SetWindowHeight(H);
+  SetPenColor(clblue);
+  a:=round((min(h,w)-50)*0.9);
+  x1:=50;
+  y1:=50;
+  x2:=50;
+  y2:=y1+a;
+  x3:=x1+a;
+  y3:=y1+a;
+  x4:=x1+a;
+  y4:=50;
+  SetPenColor(clblue);
+  MoveTo (x1, y1);
+  LineTo (x2, y2);
+  LineTo (x3, y3);
+  LineTo (x4, y4);
+  LineTo (x1 , y1);
+end.
---
+++ Labs/L_11/z4.pas
@@ -0,0 +1,17 @@
+﻿program z4;
+Uses GraphABC;
+var w,h,i,r,y:integer;
+Begin
+  r:=10;
+  w:=600;
+  h:=400;
+  y:=100;
+  SetWindowWidth(w);
+  SetWindowHeight(h);
+  SetPenColor(clblack);
+  for i:=0 to w step 30 do
+  begin
+    SetBrushColor(clRandom);
+    Circle(i,y,r)
+  end;
+end.
