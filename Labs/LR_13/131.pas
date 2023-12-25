c0a448d688a578cb3d79572ec6f0699630d9c713
Alltxe <148350310+Alltxe@users.noreply.github.com>
2023-12-25 14:03:30 +0300

    Add files via upload

---
+++ Labs/LR_13/131.pas
@@ -0,0 +1,30 @@
+﻿type
+  form = record
+    fname: string;
+    DOB: string;
+    course: 1..5;
+  end;
+
+var
+  student: form;
+
+begin
+ 
+  Write('Введите ФИО студента: ');
+  Readln(student.fname);
+  
+  Write('Введите дату рождения студента: ');
+  Readln(student.DOB);
+  
+  repeat
+    Write('Введите курс студента (от 1 до 5): ');
+    Readln(student.course);
+  until (student.course >= 1) and (student.course <= 5);
+  
+  writeln;
+  
+  Writeln('ФИО студента: ', student.fname);
+  Writeln('Дата рождения студента: ', student.DOB);
+  Writeln('Курс студента: ', student.course);
+  
+end.
---
+++ Labs/LR_13/134.pas
@@ -0,0 +1,37 @@
+﻿type
+  toy = record
+    name: string;
+    price: real;
+    age: string;
+  end;
+
+var
+  toys: array of toy;
+  i: integer;
+  f: text;
+
+begin
+  Assign(f, 'toys.txt');
+  reset(f);
+
+ 
+  SetLength(toys, 3);
+
+  for i:=0 to 2 do
+    with toys[i] do
+    begin
+      readln(f, name);
+      readln(f, price);
+      readln(f, age);
+    end;
+  
+  for i := 0 to High(toys) do
+  begin
+    writeln('Информация о товаре ', i + 1);
+    writeln('Название: ', toys[i].name);
+    writeln('Цена: ', toys[i].price:0:2);
+    writeln('Возрастной диапазон: ', toys[i].age);
+  end;
+
+  Close(f);
+end.
